4d6480b95caac2f6540ec7b3efbec01a026b88fc
Ilya Yarusov <originalyia@gmail.com>
2023-01-16 20:22:50 +0300

    bashup all config files

--- configs_backup.sh
+++ configs_backup.sh
@@ -7,10 +7,10 @@ itog="${separator}$(date)${separator}"
 
 declare -A list_directories=(
 ["alacritty"]="/home/oriyia/.config/alacritty/"
-["zsh"]="${HOME}/.zshrc"
-["i3"]="${HOME}/.config/i3/config"
-["betterlockscreen"]="home/oriyia/.config/betterlockscreenrc"
-["nvim"]="${HOME}/.config/nvim/"
+["zsh"]="/home/oriyia/.zshrc"
+["i3"]="/home/oriyia/.config/i3/config"
+["betterlockscreen"]="/home/oriyia/.config/betterlockscreenrc"
+["nvim"]="/home/oriyia/.config/nvim/"
 ["bash_history"]="/home/oriyia/.bash_history"
 ["bashrc"]="/home/oriyia/.bashrc"
 ["bash_profile"]="/home/oriyia/.profile"
@@ -18,8 +18,20 @@ declare -A list_directories=(
 ["xinitrc"]="/home/oriyia/.xinitrc"
 ["xmodmap"]="/home/oriyia/.Xmodmap"
 ["xprofile"]="/home/oriyia/.xprofile"
-["xresources"]="/home/oriyia/.Xresources")
-
+["xresources"]="/home/oriyia/.Xresources"
+["polybar"]="/home/oriyia/.config/polybar/"
+["ranger"]="/home/oriyia/.config/ranger/"
+["pygments"]="/usr/lib/python3/dist-packages/pygments/styles/my.py"
+["zathura"]="/home/oriyia/.config/zathura/zathurarc"
+["ppa"]="/etc/apt/sources.list"
+["rofi"]="/home/oriyia/.config/rofi/config.rasi"
+["rofi_theme"]="/home/oriyia/.local/share/rofi/themes/rofi_mytheme_onedark.rasi"
+["zsh_history"]="/home/oriyia/.zsh_history"
+["zshrc"]="/home/oriyia/.zshrc"
+["starship"]="/home/oriyia/.config/starship.toml"
+["lsd"]="/home/oriyia/.config/lsd/config.yaml"
+["lsd_themes"]="/home/oriyia/.config/lsd/themes/lsd_theme.yaml"
+["vivid"]="/home/oriyia/.config/vivid/vivid_mytheme_onedark.yml")
 
 target="${PWD}/saved_configs/"
 
